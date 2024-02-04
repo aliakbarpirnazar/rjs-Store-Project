91dd2592c9cba0d021c405578b1d8e59e3c35dd5
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-02-04 10:29:37 +0330

    Update V1

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Card from "../components/Card";
+import { useProducts } from "../context/ProductProvider";
+
+import styles from "./ProductsPage.module.css";
+import Loader from "../components/Loader";
 
 function ProductsPages() {
+  const products = useProducts();
   return (
-    <div>ProductsPages</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <Loader />}
+        {products.map((p) => (
+          <Card key={p.id} data={p} />
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
-export default ProductsPages
+export default ProductsPages;
--- src/services/config.js
+++ src/services/config.js
@@ -4,7 +4,7 @@ const api = axios.create({ baseURL: "https://fakestoreapi.com" });
 
 api.interceptors.response.use(
   (response) => response.data,
-  (error = Promise.reject(error))
+  (error) => Promise.reject(error)
 );
 
 export default api;
