e3184b773f756a120f6098635c26c774fc76308d
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-02-12 07:41:08 +0330

    button and ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
 import ProductProvider from "./context/ProductProvider";
+import CartProvider from "./context/CartContext";
 
 function App() {
   return (
-    <ProductProvider>
-      <Routes>
-        <Route index element={<Navigate to={"/products"} />} />
-        <Route path="/products" element={<ProductsPages />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider>
+        <Routes>
+          <Route index element={<Navigate to={"/products"} />} />
+          <Route path="/products" element={<ProductsPages />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </ProductProvider>
+    </CartProvider>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb";
+import { MdDeleteOutline } from "react-icons/md";
 
-import { shortenText } from "../helper/helper";
+import { productQuantity, shortenText } from "../helper/helper";
 
 import styles from "./Card.module.css";
+import { useCart } from "../context/CartContext";
 
 function Card({ data }) {
   const { id, title, image, price } = data;
+
+  const [state, dispatch] = useCart();
+
+  const quantity = productQuantity(state, id);
+
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
+  };
 
   return (
     <div className={styles.card}>
@@ -18,9 +28,22 @@ function Card({ data }) {
           <TbListDetails />
         </Link>
         <div>
-          <button>
-            <TbShoppingBagCheck />
-          </button>
+          {quantity === 1 && (
+            <button onClick={() => clickHandler("REMOVE_ITEM")}>
+              <MdDeleteOutline />
+            </button>
+          )}
+          {quantity > 1 && (
+            <button onClick={() => clickHandler("DECREASE")}>-</button>
+          )}
+          <span>{quantity > 0 && quantity}</span>
+          {quantity === 0 ? (
+            <button onClick={() => clickHandler("ADD_ITEM")}>
+              <TbShoppingBagCheck />
+            </button>
+          ) : (
+            <button onClick={() => clickHandler("INCREASE")}>+</button>
+          )}
         </div>
       </div>
     </div>
