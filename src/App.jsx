58cd42044d905f8c412425eb2a3b54e028e9c8a9
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-02-12 08:32:37 +0330

    product info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
+import Layout from "./layout/Layout";
 import ProductsPages from "./pages/ProductsPages";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
@@ -10,13 +12,15 @@ function App() {
   return (
     <CartProvider>
       <ProductProvider>
-        <Routes>
-          <Route index element={<Navigate to={"/products"} />} />
-          <Route path="/products" element={<ProductsPages />} />
-          <Route path="/products/:id" element={<DetailsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route index element={<Navigate to={"/products"} />} />
+            <Route path="/products" element={<ProductsPages />} />
+            <Route path="/products/:id" element={<DetailsPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Layout>
       </ProductProvider>
     </CartProvider>
   );
--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -27,7 +27,13 @@ function ProductProvider({ children }) {
 const useProducts = () => {
   const products = useContext(ProductContext);
   return products;
-};//Custom hook
+}; //Custom hook
+
+const useProductDetails = (id) => {
+  const products = useContext(ProductContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
 
 export default ProductProvider;
-export { useProducts };
+export { useProducts, useProductDetails };
